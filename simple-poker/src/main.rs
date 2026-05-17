6da955f310134b677a8682019bb07ca855d50921
s-hirano-ist <s-hirano-ist@outlook.com>
2026-05-17 16:05:56 +0900

    temp

--- simple-poker/src/main.rs
+++ simple-poker/src/main.rs
@@ -1,3 +1,36 @@
+use rand::prelude::SliceRandom;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Suit {
+    Club,
+    Diamond,
+    Heart,
+    Spade,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Card {
+    suit: Suit,
+    rank: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let suit = Suit::Club;
+    let rank = 1;
+    let card = Card { suit, rank };
+    println!("{:?}", card);
+
+    let mut deck: Vec<Card> = Vec::new();
+    let suits = [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade];
+
+    for suit in suits {
+        for rank in 1..=13 {
+            deck.push(Card { suit, rank });
+        }
+    }
+    println!("{:?}", deck);
+
+    let mut rng = rand::thread_rng();
+    deck.shuffle(&mut rng);
+    println!("{:?}", deck);
 }
